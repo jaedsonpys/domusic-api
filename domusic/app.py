db529b3c04eb2d057806cf27fea6eb672b8fa331
Jaedson Silva <contacanal798@gmail.com>
2022-11-22 00:47:02 -0300

    adding route to download video

--- domusic/app.py
+++ domusic/app.py
@@ -1,4 +1,5 @@
 import json
+import base64
 
 from mathiz import Mathiz
 from mathiz import request
@@ -33,6 +34,32 @@ def get_video_info():
         return {'status': 'error', 'msg': 'invalid_video_id'}, 400
 
     return video_info
+
+
+@app.route('/video/download')
+def download_video():
+    body = request.body
+
+    if not body:
+        return {'status': 'error', 'msg': 'no_body_content'}, 400
+    else:
+        try:
+            data = json.loads(body)
+        except json.JSONDecodeError:
+            return {'status': 'error', 'msg': 'invalid_json_data'}, 400
+
+    url = data.get('url')
+
+    if not url:
+        return {'status': 'error', 'msg': 'url_expected'}, 400
+    
+    try:
+        audio = domusic.download_audio(url)
+    except InvalidVideoIDError:
+        return {'status': 'error', 'msg': 'invalid_video_id'}, 400
+
+    b64_audio = base64.b64encode(audio.getbuffer()).decode()
+    return b64_audio, 200
 
 
 if __name__ == '__main__':
