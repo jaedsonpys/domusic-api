7f564da7bfddeba2d36e33015600021378eb7758
Jaedson Silva <contacanal798@gmail.com>
2022-11-22 00:42:18 -0300

    receiving video url in body with JSON format

--- domusic/app.py
+++ domusic/app.py
@@ -1,3 +1,5 @@
+import json
+
 from mathiz import Mathiz
 from mathiz import request
 
@@ -8,15 +10,27 @@ app = Mathiz()
 domusic = DoMusic()
 
 
-@app.route('/video/info/<video_id>')
+@app.route('/video/info')
 def get_video_info():
-    video_id = request.parameters.get('video_id')
-    url = f'https://www.youtube.com/watch?v={video_id}'
+    body = request.body
+
+    if not body:
+        return {'status': 'error', 'msg': 'no_body_content'}, 400
+    else:
+        try:
+            data = json.loads(body)
+        except json.JSONDecodeError:
+            return {'status': 'error', 'msg': 'invalid_json_data'}, 400
+
+    url = data.get('url')
+
+    if not url:
+        return {'status': 'error', 'msg': 'url_expected'}, 400
     
     try:
         video_info = domusic.get_video_info(url)
     except InvalidVideoIDError:
-        return {'status': 'error', 'error': 'invalid_video_id'}, 400
+        return {'status': 'error', 'msg': 'invalid_video_id'}, 400
 
     return video_info
 
