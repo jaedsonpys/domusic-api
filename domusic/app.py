39c8d019741deb35d0ad4a029d5beba072f36f34
Jaedson Silva <contacanal798@gmail.com>
2022-11-21 23:54:37 -0300

    adding route to get video info

--- domusic/app.py
+++ domusic/app.py
@@ -1,7 +1,18 @@
 from mathiz import Mathiz
 from mathiz import request
 
+from .domusic import DoMusic
+
 app = Mathiz()
+domusic = DoMusic()
+
+
+@app.route('/video/info/<url>')
+def get_video_info():
+    url = request.parameters.get('url')
+    domusic.get_video_info(url)
+    return domusic
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
