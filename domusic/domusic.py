26d53ffc4574acf3ba2e6d0bb798f2829b307b8f
Jaedson Silva <contacanal798@gmail.com>
2022-11-21 22:01:00 -0300

    creating method to get best audio quality

--- domusic/domusic.py
+++ domusic/domusic.py
@@ -2,17 +2,22 @@ import pytube
 
 
 class DoMusic:
-    def get_video_info(self, url: str) -> dict:
-        yt = pytube.YouTube(url)
-        all_videos = yt.streams.filter(file_extension='mp4', only_audio=True)
-
+    def get_best_audio_quality(self, all_videos: pytube.StreamQuery) -> pytube.Stream:
         max_filesize = 0
-        best_video: pytube.Stream = None
+        best_video = None
 
         for video in all_videos:
             if video.filesize > max_filesize:
                 max_filesize = video.filesize
                 best_video = video
+
+        return best_video
+
+    def get_video_info(self, url: str) -> dict:
+        yt = pytube.YouTube(url)
+        all_videos = yt.streams.filter(file_extension='mp4', only_audio=True)
+
+        best_audio = self.get_best_audio_quality(all_videos)
 
         author = yt.author
         views = yt.views
@@ -22,8 +27,8 @@ class DoMusic:
             'author': author,
             'views': views,
             'thumbnail_url': thumbnail_url,
-            'title': best_video.title,
-            'size': best_video.filesize,
+            'title': best_audio.title,
+            'size': best_audio.filesize,
             'url': yt.watch_url
         }
 
